138de40d55a63797464411e75d04ad890e084198
hyeong.kwak <gudrb2361@gmail.com>
2020-07-26 16:21:27 +0900

    로그인 post 추가

--- Api/User.js
+++ Api/User.js
@@ -4,14 +4,33 @@ const User = require('../DB/User');
 const route = express.Router();
 
 route.post('/', async (req, res) => {
-    const { token } = req.body;
-    let user = {};
-    // user.firstName = firstName;
-    // user.lastName = lastName;
-    user.token = token;
-    let userModel = new User(user);
-    await userModel.save();
-    res.json(userModel);
+    if (req.body.type === "add") {
+        const { token } = req.body;
+        let user = {};
+        // user.firstName = firstName;
+        // user.lastName = lastName;
+        user.token = token;
+        let userModel = new User(user);
+        await userModel.save();
+        res.json(userModel);
+    } else {
+        await User.findOne({
+            token: req.body.token
+        }).then(user => {
+            if(!user) {
+                res.status(200).send({
+                    msg: 'not! token!',
+                    success: false
+                });
+            } else {
+                res.status(200).send({
+                    msg: 'yes! token!',
+                    success: true
+                })
+            }
+        });
+
+    }
 });
 
 route.get('/', (req, res) => {
